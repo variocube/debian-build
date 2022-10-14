0595d77546aa3ac5fc927074cd544f2a82121759
Peter Karrer <peter@karrer.pro>
2022-10-14 14:28:53 +0200

    feat: support for different releases

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,27 @@
 #
 # Dockerfile for an image that containing build tools for Debian packages using nodejs.
 #
+ARG RELEASE
+FROM debian:${RELEASE}-slim
+ARG RELEASE
+ARG NODE
 
-FROM debian:bullseye-slim
+# Add backports to stretch to give it high priority
+RUN if [ "$RELEASE" = "stretch" ]; then \
+      echo "deb http://ftp.debian.org/debian stretch-backports main" >> /etc/apt/sources.list; \
+      echo "Package: *\nPin: release a=stretch-backports\nPin-Priority: 500" >> /etc/apt/preferences; \
+    fi
 
-RUN apt-get update
-RUN apt-get install -y --no-install-recommends curl ca-certificates
-RUN curl -fsSL https://deb.nodesource.com/setup_14.x | bash -
-RUN apt-get update
-RUN apt-get install -y --no-install-recommends nodejs devscripts build-essential debhelper-compat
-RUN apt-get clean
+# Install node and build tools
+RUN apt-get update \
+    && apt-get install -y --no-install-recommends curl ca-certificates
+RUN curl -fsSL https://deb.nodesource.com/setup_$NODE.x | bash -
+RUN apt-get update \
+    && apt-get install -y --no-install-recommends nodejs devscripts build-essential debhelper
+
+# Install python2.7 on stretch
+RUN if [ "$RELEASE" = "stretch" ]; then \
+      apt-get install -y --no-install-recommends python2.7; \
+    fi
+
+RUN apt-get clean && rm -rf /var/lib/apt/lists/*
--- build.sh
+++ build.sh
@@ -8,9 +8,36 @@
 
 # You might need to adapt the login for your setup.
 # For this to work a profile named "AdministratorAccess-Variocube" must be present in ~/.aws/config
+# See https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-sso.html
+# Also, you might need to login to this profile first:
+#   aws sso login --profile AdministratorAccess-Variocube
 aws ecr-public get-login-password --region us-east-1 --profile AdministratorAccess-Variocube | docker login --username AWS --password-stdin public.ecr.aws/variocube
 
 # You might need to install Dockers buildx extension on your system.
 docker buildx create --name debian-build-builder
 docker buildx use debian-build-builder
-docker buildx build --platform linux/arm,linux/amd64 -t public.ecr.aws/variocube/debian-build --push .
+
+# stretch-based image with Node 14
+docker buildx build \
+  --build-arg RELEASE=stretch \
+  --build-arg NODE=14 \
+  --platform linux/arm,linux/amd64 \
+  -t public.ecr.aws/variocube/debian-build:stretch \
+  --push .
+
+# bullseye-based image with Node 18
+docker buildx build \
+  --build-arg RELEASE=bullseye \
+  --build-arg NODE=18 \
+  --platform linux/arm,linux/amd64 \
+  -t public.ecr.aws/variocube/debian-build:bullseye \
+  --push .
+
+# latest image is currently bullseye
+docker buildx build \
+  --build-arg RELEASE=bullseye \
+  --build-arg NODE=18 \
+  --platform linux/arm,linux/amd64 \
+  -t public.ecr.aws/variocube/debian-build:latest \
+  --push .
+
